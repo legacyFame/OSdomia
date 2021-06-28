1d6f7a59640d77ad344232cefda020dd95aed925
Hiro <laciferin@gmail.com>
2021-06-28 13:32:25 +0530

    Rest

--- systemCalls.c
+++ systemCalls.c
@@ -1,7 +1,20 @@
-
-int main() {
-    printf("Hi1");
-    return 1;
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+ 
+int main()
+{
+    pid_t cpid;
+    if (fork()== 0){
+        printf("Child is running\n");
+        printf("Pid %d\n", getpid());
+        printf("Parent %d\n", getppid());
+        exit(0);
+    }
+    else
+        cpid = wait(NULL); //Cascading termination
+    printf("Main pid %d\n", getpid());
+    printf("Child pid procured after waiting %d\n", cpid);
+    return 0;
 }
-
-
