a618081f454666f669a86aadfd825a4b4631f369
Hiro <laciferin@gmail.com>
2021-06-28 07:50:56 +0530

    cmake-build-debug/CMakeFiles/OSdomia.dir/cmake_clean.cmake

---
+++ cmake-build-debug/CMakeFiles/OSdomia.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSdomia.dir/main.c.obj"
+  "OSdomia.exe"
+  "OSdomia.exe.manifest"
+  "OSdomia.pdb"
+  "libOSdomia.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSdomia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
