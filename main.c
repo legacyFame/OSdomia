82ed45fa752775ac24aacda6eeac5bb6df7577e9
Nasfame <laciferin@gmail.com>
2022-02-28 17:25:27 +0530

    GIT push

--- main.c
+++ main.c
@@ -11,23 +11,6 @@ typedef struct Pager Pager;
 typedef struct Map Map;
 typedef struct Mapper Mapper;
 
-struct Pager
-{
-    int capacity;
-    int i;
-    int** arr;
-};
-
-bool in_pager( Pager page,int no){
-    for(int i=0;i<page.i;i++)
-     if(page.arr[i]==no) return true;
-    return false;
-}
-
-bool overflow(Pager page){
-    return page.i>=page.capacity;
-}
-
 struct Map{
     int key;
     int val;
@@ -39,17 +22,47 @@ struct Mapper{
     Map **maps;
 };
 
-void push(Mapper* m,int key,int val){
+struct Pager
+{
+    int capacity;
+    int i;
+    int** pages;
+    Mapper *map;
+};
+
+bool in_pager( Pager *p,int no){
+    for(int i=0;i<p->i;i++)
+     if(p->pages[i]==no) return true;
+    return false;
+}
+
+bool overflow(Pager *page){
+    return page->i>=page->capacity;
+}
+
+void push_page(Pager *p,int page){
+    push_map(p->map,page,p->i);
+    p->pages[p->i]= page;
+}
+
+Map* get_map( Mapper *m,int no){
+    Map *least = {-1,9999};
+    for(int i=0;i<m->i;i++)
+     if(m->maps[i]==no) return m->maps[i];
+    return least;
+}
+
+bool in_map(Mapper *m,int k){
+    Map* m1  = get_map(m,k);
+}
+
+void push_map(Mapper* m,int key,int val){
     int i = m->i;
-    if(i>=m->capacity) printf("Overflowing Mapper\n");
-    else
-    {
-      Map *map = (Map*) malloc(sizeof(Map));
-      map->key = key;
-      map->val = val;
-      m->maps[i] = map;
-      m->i++;    
-    }
+    Map *map = (Map*) malloc(sizeof(Map));
+    map->key = key;
+    map->val = val;
+    m->maps[i] = map;
+    m->i++;    
 }
 
 int get(Mapper *m,int key){
@@ -58,19 +71,51 @@ int get(Mapper *m,int key){
     return 0;    
 }
 
+Map* lru(Mapper *m)
+{
+    Map *least = {-1,9999};
+    
+    if(m->i ==0) push(m,-1,-1);
+
+    for (int i=0;i<m->i;i++){
+         Map *map = m->maps[i];
+         least = map ? map->val<least->val | least->key==-1: least;
+    }
+    return least;
+}
+
+void faults(int req[],int size, Pager *p, Mapper *m){
+
+    for(int i=0;i<size;i++){
+        if(overflow(p)){
+
+        }
+        else
+        {
+            /* code */
+        }
+        
+    }
+}
+
+
 
 void main(){
     printf("\n");
+    int arr = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2};
+    int size = 13;
     // char *ini = input('Page Request Array');
-    // char** arr = split(ini," ");
     // int size = toInt(input("Whats the size of the Array?"));
     // int *reqs = toIntArray(arr,size);
     // int capacity = toInt(input("Whats the capacity"));
-    // int page_faults=0;
+    int page_faults=0;
     int capacity=4;
-    Pager P1 = {capacity,1,malloc(sizeof(int*)*capacity)};
-    Mapper M1 = {capacity,0,malloc(sizeof(Map)*capacity),&push};
-    push(&M1,1,2);
+    Mapper *M1 = {size,0,malloc(sizeof(Map)*size)};
+    Pager P1 = {capacity,0,malloc(sizeof(int)*capacity),M1};
+
+
+
+    // push(&M1,1,2);
     // get(&M1,1);
     // printf("%d",get(&M1,1));
 }
